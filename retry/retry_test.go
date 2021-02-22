62db0d1c3664ee852a8e9e48725d9f19b69496c5
fredy <fredy@163.com>
2021-02-22 16:10:51 +0800

    update retry params

--- retry/retry_test.go
+++ retry/retry_test.go
@@ -12,10 +12,10 @@ func TestRetry(t *testing.T) {
 	now := time.Now()
 
 	// fibonacci
-	NewRetry(
-		WithRetry(5),
-		WithInterval(time.Millisecond*10),
-	).Do(func() error {
+	NewRetry(Options{
+		Retries:  5,
+		Interval: time.Millisecond * 10,
+	}).Do(func() error {
 		now2 := time.Now()
 		log.Printf("fibonacci ms %v", (now2.UnixNano()-now.UnixNano())/1e6)
 		now = now2
@@ -25,9 +25,10 @@ func TestRetry(t *testing.T) {
 	now = time.Now()
 
 	// exponent
-	NewRetry(
-		WithBackoff(ExponentialBackoff, time.Millisecond),
-	).Do(func() error {
+	NewRetry(Options{
+		BackOff:  ExponentialBackoff,
+		Interval: time.Millisecond,
+	}).Do(func() error {
 		log.Printf("exponent ms %v", (time.Now().UnixNano()-now.UnixNano())/1e6)
 		return errors.New("testerror")
 	})
@@ -35,9 +36,10 @@ func TestRetry(t *testing.T) {
 	now = time.Now()
 
 	// average
-	NewRetry(
-		WithBackoff(AverageBackOff, time.Millisecond*10),
-	).Do(func() error {
+	NewRetry(Options{
+		BackOff:  AverageBackOff,
+		Interval: time.Millisecond * 10,
+	}).Do(func() error {
 		now2 := time.Now()
 		log.Printf("average ms %v", (time.Now().UnixNano()-now.UnixNano())/1e6)
 		now = now2
@@ -45,9 +47,10 @@ func TestRetry(t *testing.T) {
 	})
 
 	// increase
-	err := NewRetry(
-		WithBackoff(IncreaseBackOff, time.Millisecond*10),
-	).Do(func() error {
+	err := NewRetry(Options{
+		BackOff:  IncreaseBackOff,
+		Interval: time.Millisecond * 10,
+	}).Do(func() error {
 		now2 := time.Now()
 		log.Printf("increase ms %v", (time.Now().UnixNano()-now.UnixNano())/1e6)
 		now = now2
@@ -62,10 +65,11 @@ func TestRetry(t *testing.T) {
 		}
 		return true, nil
 	}
-	NewRetry(
-		WithCheck(check),
-		WithBackoff(AverageBackOff, time.Millisecond*10),
-	).Do(func() error {
+	NewRetry(Options{
+		Check:    check,
+		BackOff:  AverageBackOff,
+		Interval: time.Millisecond * 10,
+	}).Do(func() error {
 		now2 := time.Now()
 		log.Printf("check ms %v", (time.Now().UnixNano()-now.UnixNano())/1e6)
 		now = now2
